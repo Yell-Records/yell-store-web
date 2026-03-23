d6049fe2aa1035bcfa4efc505cb6390abb658580
Matthew Cumbo <mattcumbo99@gmail.com>
2026-03-23 15:16:22 -0500

    Added new tab icon and banner image (#96)
    Co-authored-by: MattCumbo99 <mattcumbo99@users.noreply.github.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,6 +20,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Quantum Mart');
+    expect(compiled.querySelector('.banner-img')).toBeTruthy();
   });
 });
