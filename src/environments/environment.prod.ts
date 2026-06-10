0e500f72788c5d95372b058bfa733529fe55a329
MattCumbo99 <mattcumbo99@users.noreply.github.com>
2026-06-10 14:25:36 -0500

    Prod config adjusted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.yellrecords.com/api',
+  apiUrl: 'https://2hr6tpeqb0.execute-api.us-east-2.amazonaws.com/api',
   paypalClientId:
     'Ac_zOOqtkb8ClgOGvSkPmcsJrsZPwqWk_ypEZmnlcRTQxKKDXIJ0U4BFuZTkFgoYiu97Em5qoMzhYlJa',
 };
